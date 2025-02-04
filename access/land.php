1b4582d4ac95b2dfdf0ce16389b171d1765b29f4
SMars24 <sophiamars24@gmail.com>
2025-02-04 12:14:49 -0800

    land reg button
    if it deletes thats my bad !

--- access/land.php
+++ access/land.php
@@ -129,14 +129,12 @@ if (!isset($_SESSION['user_id'])) {
             <div class="row">
                 
 				<div class="col-md-4 mb-4">
-                <a href="/access/registration.php" class="text-decoration-none">
                     <div class="feature-card h-100 text-center">
                         <div class="feature-icon">🔒</div>
                         <h3>Registration</h3>
                         <p>Begin Your Journey</p>
                     </div>
-                </a>
-            </div>
+                </div>
 				
 				<div class="col-md-4 mb-4">
                     <div class="feature-card h-100 text-center">
