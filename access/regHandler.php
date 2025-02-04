b981488a4e770f4c0e9fd1f213e8b9981ce48d1c
EduardoVN15 <eduardoverdin2008@gmail.com>
2025-02-04 11:55:12 -0800

    Deleted code
    Deleted student Id and grade level

--- access/regHandler.php
+++ access/regHandler.php
@@ -10,8 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
     $confirmPassword = trim($_POST['confirmPassword']);
-	$gradeLevel = trim($_POST['gradeLevel']);
-	$studentID = trim($_POST['studentID']);
+	
 
     $errors = [];
 
@@ -19,8 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (empty($email)) $errors[] = "Email is required.";
     if (empty($password)) $errors[] = "Password is required.";
     if ($password !== $confirmPassword) $errors[] = "Passwords do not match.";
-	if (empty($gradeLevel)) $errors[] = "gradeLevel is required.";
-	if (empty($studentID)) $errors[] = "studentID is required.";
+
 
     if (!empty($errors)) {
         $_SESSION['errors'] = $errors;
@@ -33,8 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt->bindParam(':username', $username, PDO::PARAM_STR);
     $stmt->bindParam(':email', $email, PDO::PARAM_STR);
     $stmt->bindParam(':password', $password, PDO::PARAM_STR);
-	$stmt->bindParam(':gradeLevel', $gradelevel, PDO::PARAM_STR);
-    $stmt->bindParam(':studentID', $studentID, PDO::PARAM_STR);
+	
 
     $stmt->execute();
     header("Location: login.php");
