0f9d6887237a99f728aa8da56858acf9d34d13a3
SMars24 <sophiamars24@gmail.com>
2025-03-27 13:01:47 -0700

    ???

--- access/land.php
+++ access/land.php
@@ -125,12 +125,10 @@ session_start();
                         </div>
                     </a>
                 </div>
-
             </div>
         </div>
     </section>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
-
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
